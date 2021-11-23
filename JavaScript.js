91fe989cccb00c7fa0f7de7dddd500e23033dc39
naamaah <naamaah@post.bgu.ac.il>
2021-11-23 19:03:11 +0200

    targil5

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+function thankyou()
+    {
+        document.getElementById("button").innerHTML = "Thank You";
+        console.log("Naama Aharony");
+
+    }
+
+
+function changePic(){
+    document.getElementById("pic").src = "Naama.jpeg";
+}
